3d1ffe5ba2f082f96701dcc2ec275c2373f18e4d
Michael Doudy <michael.doudy@gmail.com>
2020-06-11 14:37:06 -0400

    Added dbSave and dbDelete to moveiModel.js, refactored sql schema

--- server/models/movieModel.js
+++ server/models/movieModel.js
@@ -1,10 +1,16 @@
-//Select one db to work with:
-
 //For SQL
 const sqlDb = require('../../db/sql');
 //For Mongo
 const mongoDb = require('../../db/mongodb')
 
-module.exports = {
+module.exports.dbSave = ({title, vote_average, release_date, genre, poster_path, overview}) => {
+  sqlDb.query(`SELECT * FROM favorites WHERE title = ?;`, [title], (err, data) => {
+    if (!err && !data.length) {
+      sqlDb.query(`INSERT INTO favorites (title, vote_average, release_date, genre, poster_path, overview) VALUES (?, ?, ?, ?, ?, ?);`, [title, vote_average, release_date, genre, poster_path, overview])
+    }
+  });
+}
 
+module.exports.dbDelete = ({title}) => {
+  sqlDb.query(`DELETE FROM favorites WHERE title = ?;`, [title]);
 }
